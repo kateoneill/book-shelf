5be902376094b8b80ac95d3087db9ae979c28f9a
kateoneill <20088535@mail.wit.ie>
2022-11-16 15:27:28 +0000

    Add skeleton menu with CRUD headings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,74 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import utils.ScannerInput
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    runMenu();
+}
+
+fun mainMenu() : Int {
+    return ScannerInput.readNextInt("""
+          > ----------------------------------
+          > |  Author MENU                   |
+          > |   1) Add an author             |
+          > |   2) List all authors          |
+          > |   3) Update author             |
+          > |   4) Delete an author          |
+          > ----------------------------------
+          > |  Book MENU                     |
+          > |   5) Add a book to author      |
+          > |   6) Update book               |
+          > |   7) Delete book               |
+          > ----------------------------------
+          > |   0) Exit                      |
+          > ----------------------------------
+          > Choose:""".trimMargin(">"))
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1 -> addAuthor()
+            2 -> listAuthors()
+            3 -> updateAuthor()
+            4 -> deleteAuthor()
+            5 -> addBook()
+            6 -> updateBook()
+            7 -> deleteBook()
+            0 -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    }while (true)
+}
+
+fun addAuthor() {
+    println("Author added")
+}
+
+fun updateAuthor(){
+    println("Updating author")
+}
+
+fun deleteAuthor(){
+    println("deleting author")
+}
+
+fun listAuthors(){
+    println("list all authors")
+}
+
+fun addBook(){
+    println("Add book")
+}
+
+fun deleteBook(){
+    println("delete book")
+}
+
+fun updateBook(){
+    println("update book")
+}
+fun exitApp(){
+    println( "Bye bye, come back soon 👋")
+    println("exitApp() function invoked")
+    System.exit(0)
 }
