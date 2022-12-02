051e4ad54f04d8a8d43505302dc0587fb0f2faaa
kateoneill <20088535@mail.wit.ie>
2022-12-02 22:23:27 +0000

    Add tests for count authors by publisher

--- src/test/kotlin/controllers/AuthorAPITest.kt
+++ src/test/kotlin/controllers/AuthorAPITest.kt
@@ -269,4 +269,23 @@ class AuthorAPITest {
             assertFalse(searchResults.contains("Colson"))
         }
     }
+
+    @Nested
+    inner class CountingMethods {
+
+        @Test
+        fun numberOfNotesCalculatedCorrectly() {
+            assertEquals(6, populatedAuthors!!.numberOfAuthors())
+            assertEquals(0, emptyAuthors!!.numberOfAuthors())
+        }
+
+        @Test
+        fun numberOfNotesByPublisherCalculatedCorrectly() {
+            assertEquals(2, populatedAuthors!!.numberOfAuthorsByPublisher("Simon & Schuster"))
+            assertEquals(2, populatedAuthors!!.numberOfAuthorsByPublisher("Penguin"))
+            assertEquals(1, populatedAuthors!!.numberOfAuthorsByPublisher("Faber & Faber"))
+            assertEquals(1, populatedAuthors!!.numberOfAuthorsByPublisher("Little, Brown and Company"))
+            assertEquals(0, emptyAuthors!!.numberOfAuthors())
+        }
+    }
 }
